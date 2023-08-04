b45ba983f54549d849b9aa82b5bce9de454c251a
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-08-04 21:07:47 +0530

    multiple data show

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,11 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 8, 3);
-    const expenseTitle = 'Car Insaurance';
-    const expenseAmount = 294.67;
+function ExpenseItem(props) {    
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toDateString()}</div>
+            <div>{props.date.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     )
