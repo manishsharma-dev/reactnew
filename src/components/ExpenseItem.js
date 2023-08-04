30844a8ffc5a0a6eb2a0ca1fe55da8aebb2b1adf
Manish Sharma <manish.sharma@ndlm.co.in>
2023-08-04 13:29:20 +0530

    display dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import './ExpenseItem.css';
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 8, 3);
+    const expenseTitle = 'Car Insaurance';
+    const expenseAmount = 294.67;
     return (
         <div className='expense-item'>
-            <div>March 28th 2021</div>
+            <div>{expenseDate.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insaurance</h2>
-                <div className='expense-item__price'>$294.97</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
             </div>
         </div>
     )
