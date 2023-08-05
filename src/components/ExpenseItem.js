d9ef84d62f493a92ddaee04912ff338f4f50f647
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-08-06 00:23:17 +0530

    passing props to multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
-import './ExpenseItem.css';
-function ExpenseItem(props) {    
-    return (
-        <div className='expense-item'>
-            <div>{props.date.toDateString()}</div>
-            <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-        </div>
-    )
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
