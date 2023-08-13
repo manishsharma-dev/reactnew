2f922f51a32c34ed6b1a7d14b3963e413ed45256
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-08-13 21:00:02 +0530

    conditional jsx rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 function Expenses(props) {
   const [filteredyear, setFilteredYear] = useState("");
   const expenses = props.expenses;
@@ -12,6 +12,7 @@ function Expenses(props) {
   const filteredExpenses = expenses.filter(
     (expense) => expense.date.getFullYear().toString() === filteredyear
   );
+
   return (
     <div>
       <Card className="expenses">
@@ -19,16 +20,7 @@ function Expenses(props) {
           selectedYear={filteredyear}
           onChangeFilterHandler={changeFilterHandler}
         />
-        {filteredExpenses.length === 0 && <p>No Expenses Found </p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))}
+        <ExpensesList filteredExpenses={filteredExpenses} />
       </Card>
     </div>
   );
