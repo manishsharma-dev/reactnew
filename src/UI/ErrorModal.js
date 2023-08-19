ec9fb64fd90d5c37e4c209586b67e47cbeb4d378
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-08-19 14:17:48 +0530

    portals and refs

--- src/UI/ErrorModal.js
+++ src/UI/ErrorModal.js
@@ -1,23 +1,43 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
-function ErrorModal(props) {  
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
+};
+
+const ModalOverlay = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onClose} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onClose}>Ok</Button>
-        </footer>
-      </Card>
-    </div>
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onClose}>Ok</Button>
+      </footer>
+    </Card>
+  );
+};
+function ErrorModal(props) {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onClose={props.onClose}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 }
 
--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -6,13 +6,17 @@ import ErrorModal from "../UI/ErrorModal";
 import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
+  const usernameRef = useRef();
+  const ageRef = useRef();
   const [userInput, setUserInput] = useState(DEFAULT_INPUT_STATE);
   const [dialogContent, setDialogContent] = useState(DEFAULT_DIALOG_STATE);
   const addUserHandler = (event) => {
     event.preventDefault();
+    const enteredName = usernameRef.current.value;
+    const enteredAge = ageRef.current.value;
     if (
-      userInput.username.trim().length === 0 ||
-      userInput.age.trim().length === 0
+      enteredName.trim().length === 0 ||
+      enteredAge.trim().length === 0
     ) {
       setDialogContent({
         title: "Invalid Input",
@@ -20,21 +24,27 @@ const AddUser = (props) => {
       });
       return;
     }
-    if (isNaN(userInput.age.trim()) || +userInput.age < 1) {
+    if (isNaN(enteredAge.trim()) || +enteredAge < 1) {
       setDialogContent({
         title: "Invalid Age",
         message: "Please enter a valid age (valid positive number)",
       });
       return;
     }
+    setUserInput({
+      username: enteredName,
+      age: enteredAge
+  })
     setDialogContent(DEFAULT_DIALOG_STATE);
     props.onFetchUserData(userInput);
+    enteredName.current.value = '';
+    enteredAge.current.value = '';
     setUserInput(DEFAULT_INPUT_STATE);
   };
   const onInputHandler = (input, value) => {
     setUserInput((prevState) => {
       return { ...prevState, [input]: value };
-    });    
+    });
   };
   const closeModal = () => {
     setDialogContent(DEFAULT_DIALOG_STATE);
@@ -59,6 +69,7 @@ const AddUser = (props) => {
               onChange={(event) =>
                 onInputHandler("username", event.target.value)
               }
+              ref={usernameRef}
             />
             <label htmlFor="age">Age (Years)</label>
             <input
@@ -66,6 +77,7 @@ const AddUser = (props) => {
               type="number"
               value={userInput.age || ""}
               onChange={(event) => onInputHandler("age", event.target.value)}
+              ref={ageRef}
             />
             <Button type="submit" onClick={addUserHandler}>
               Add User
