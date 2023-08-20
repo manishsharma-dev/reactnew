378479811c336057365a904a542e56ff78a30907
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-08-20 14:52:49 +0530

    context with useContext

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import AuthContext from '../../store/auth-context';
-import classes from './Navigation.module.css';
+import React, { useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-    {(ctx) => {
-      return (
-        <nav className={classes.nav}>
+    <nav className={classes.nav}>
       <ul>
         {ctx.isLoggedIn && (
           <li>
@@ -21,15 +19,11 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
     </nav>
-      )
-    }}
-    
-    </AuthContext.Consumer>
   );
 };
 
