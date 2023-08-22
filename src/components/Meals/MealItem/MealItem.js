537d5658faf8b5333bc8bec557e31c3db60b6be6
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-08-22 21:50:36 +0530

    Chapter 179

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "../MealItemForm/MealItemForm";
+import CartContext from "../../../store/cart-context";
 const MealItem = (props) => {
+  const cartContext = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartContext.addItem({
+      id: props.id,
+      name : props.name,
+      amount : amount,
+      price : props.price
+    })
+  };
   return (
     <li className={classes.meal}>
       <div>
@@ -9,7 +21,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm onAddToCart={addToCartHandler} />
+      </div>
     </li>
   );
 };
---
+++ src/store/CartProvider.js
@@ -0,0 +1,49 @@
+import { useReducer } from "react";
+import CartContext from "./cart-context";
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    default:
+      return INITIAL_CURRENT_STATE;
+  }
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    INITIAL_CURRENT_STATE
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
+
+const INITIAL_CURRENT_STATE = {
+  items: [],
+  totalAmount: 0,
+};
