2adaf5280b6932cdbecb9810e820250ba6110252
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-08-24 20:32:45 +0530

    use memo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import React, { useState, useCallback } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
+import Demo from "./components/Demo/Demo";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToogle, setAllowToggle] = useState(false);
+  console.log("App running");
+  const toggleParagraph = useCallback(() => {
+    if (allowToogle) {
+      setShowParagraph((prevState) => !showParagraph);
+    }
+  }, [allowToogle,showParagraph]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
-    <div className="App">      
+    <div className="app">
+      <h1>Hi there!</h1>
+      <Demo show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraph}>Show Paragraph</Button>
     </div>
   );
 }
