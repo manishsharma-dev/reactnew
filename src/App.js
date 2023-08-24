f02e442023f589ce6b9802b76c982c5cd6261852
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-08-24 21:02:36 +0530

    using fetch for api calls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import './App.css';
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
+  const [dummyMovies, setDummyMovies] = useState([]);
+  function fetchMovieHandler() {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.results);
+        const transformedMovies = data.results.map((movie) => {
+          return {
+            id : movie.episode_id,
+            title : movie.title,
+            openingText : movie.opening_crawl,
+            releaseDate : movie.release_date
+          };
+        });
+        setDummyMovies(transformedMovies);
+      });
+  }
   return (
-    <div className="App">      
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
 
