bf54dad2f0616607a3dde5010cb771983eec16ac
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-09-02 14:17:20 +0530

    common api call as custom hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
+  const { isLoading, error, sendRequest : fetchTasks } = useHttp(); 
+  const [tasks, setTasks] = useState([]);
+  // const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  const transformTasks = useCallback((tasksObj) => {
+    const loadedTasks = [];
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+    setTasks(loadedTasks);
+  },[]);
+ 
+
+  useEffect(() => {
+    fetchTasks({
+      url: "https://react-http-6c75f-default-rtdb.firebaseio.com/tasks.json",
+    }, transformTasks);
+  }, [fetchTasks,transformTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
-    <div className="App">      
-    </div>
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
   );
 }
 
---
+++ src/hooks/use-http.js
@@ -0,0 +1,39 @@
+import { useCallback, useState } from "react";
+const useHttp = ( ) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig,applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        requestConfig.url,
+        {
+          method: requestConfig.method ?? "GET",
+          headers: requestConfig.headers ?? {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        }
+        // 'https://react-http-6c75f-default-rtdb.firebaseio.com/tasks.json'
+      );
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  },[]);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
