1cc2eb95da3a7d00a94a0894ea7ed0670075a0c9
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-09-04 22:16:30 +0530

    show form error on submit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
   return (
-    <div className="App">      
+    <div className="app">
+      <SimpleInput />
     </div>
   );
 }
