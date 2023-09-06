fd7acb9e7aaff8de1f9b4b42820e03e950420c80
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-09-06 22:32:40 +0530

    api call for meals

--- src/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./AvailableMeals.module.css";
 import Card from "../../UI/Card/Card";
 import MealItem from "../MealItem/MealItem";
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [mealItemList, setMealItem] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch(
+        "https://react-http-6c75f-default-rtdb.firebaseio.com/meals.json1"
+      );
+      var data = [];
+      const loadedData = [];
+      if (!response.ok === 200) {
+        throw new Error("Something went wrong");        
+      } 
+      data = await response.json();
+      for (const key in data) {
+        loadedData.push({
+          id: key,
+          ...data[key],
+        });
+      }
+      setMealItem(loadedData);
+    }
+    try {
+      fetchMeals();
+    } catch (err) {
+      setIsLoading(false);
+      setHttpError(err.message);
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+  if(httpError){
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = mealItemList.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -23,29 +68,3 @@ const AvailableMeals = () => {
 
 export default AvailableMeals;
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
