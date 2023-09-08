e0b5848b9aa271edf85650bf3670517d259097ca
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-09-08 21:13:21 +0530

    dispatch and useSelector

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,23 @@
-import redux, { createStore } from 'redux';
+import  { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    else if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    return state
-}
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  else if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
 
 const store = createStore(counterReducer);
 
+export default store;
