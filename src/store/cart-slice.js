54956994b9eca96f70e70c1a9057819d0fe32f94
Manish Sharma <sharmakmanish1@gmail.com>
2023-09-15 22:07:39 +0530

    redux-toolkit completed

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,18 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState: {
     items: [],
     totalQuantity: 0,
-    totalAmount: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItems = state.items.find((item) => item.id === newItem.id);
+      const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
-      if (!existingItems) {
+      state.changed = true;
+      if (!existingItem) {
         state.items.push({
           id: newItem.id,
           price: newItem.price,
@@ -21,19 +26,20 @@ const cartSlice = createSlice({
           name: newItem.title,
         });
       } else {
-        existingItems.quantity++;
-        existingItems.totalPrice += newItem.price;
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice -= existingItem.price;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
